667eab9657153a632a4ad0f9216f4a21fb9dac89
Christian Aigner <github@sys-admin.at>
2017-04-02 11:44:10 +0200

    Ported exifsort.sh to use graphicsmagick & exiftool + minor bug fixes.

--- exifsort.sh
+++ exifsort.sh
@@ -28,11 +28,11 @@ USE_FILE_EXT=TRUE
 # cause .jpg to be used instead of .jpeg as the file extension. If FALSE (or any other
 # value) .jpeg is used instead. This is only used if USE_FILE_EXT is TRUE and used.
 #
-JPEG_TO_JPG=FALSE
+JPEG_TO_JPG=TRUE
 #
 #
 # The following is an array of filetypes that we intend to locate using find.
-# Any imagemagick-supported filetype can be used, but EXIF data is only present in
+# Any graphicsmagick-supported filetype can be used, but EXIF data is only present in
 # jpeg and tiff. Script will optionally use the last-modified time for sorting (see above)
 # Extensions are matched case-insensitive. *.jpg is treated the same as *.JPG, etc.
 # Can handle any file type; not just EXIF-enabled file types. See USE_LMDATE above.
@@ -45,6 +45,11 @@ FILETYPES=("*.jpg" "*.jpeg" "*.png" "*.tif" "*.tiff" "*.gif" "*.xcf")
 # FIXME: Gracefully handle unavailable destinations, non-trailing slash, etc.
 #
 MOVETO=""
+#
+# Directory name format (as used in the date command) used in the file move 
+#
+DIRFORMAT="+%Y/%m/%Y%m%d/"
+#
 #
 ###############################################################################
 # End of settings. If you feel the need to modify anything below here, please share
@@ -65,58 +70,71 @@ MOVETO=""
 #
 # Are we supposed to run an action? If not, skip this entire section.
 if [[ "$1" == "doAction" && "$2" != "" ]]; then
- # Check for EXIF and process it
- echo -n ": Checking EXIF... "
- DATETIME=`identify -verbose "$2" | grep "exif:DateTime:" | awk -F' ' '{print $2" "$3}'`
- if [[ "$DATETIME" == "" ]]; then
- echo "not found."
- if [[ $USE_LMDATE == "TRUE" ]]; then
- # I am deliberately not using %Y here because of the desire to display the date/time
- # to the user, though I could avoid a lot of post-processing by using it.
- DATETIME=`stat --printf='%y' "$2" | awk -F. '{print $1}' | sed y/-/:/`
- echo " Using LMDATE: $DATETIME"
- else
- echo " Moving to ./noexif/"
- mkdir -p "${MOVETO}noexif" && mv -b -f "$2" "${MOVETO}noexif"
- exit
- fi;
- else
- echo "found: $DATETIME"
- fi;
- # The previous iteration of this script had a major bug which involved handling the
- # renaming of the file when using TS_AS_FILENAME. The following sections have been
- # rewritten to handle the action correctly as well as fix previously mangled filenames.
- # FIXME: Collisions are not handled.
- #
- EDATE=`echo $DATETIME | awk -F' ' '{print $1}'`
- # Evaluate the file extension
- if [ "$USE_FILE_EXT" == "TRUE" ]; then
- # Get the FILE type and lowercase it for use as the extension
- EXT=`file -b $2 | awk -F' ' '{print $1}' | tr '[:upper:]' '[:lower:]'`
- if [[ "${EXT}" == "jpeg" && "${JPEG_TO_JPG}" == "TRUE" ]]; then EXT="jpg"; fi;
- else
- # Lowercase and use the current extension as-is
- EXT=`echo $2 | awk -F. '{print $NF}' | tr '[:upper:]' '[:lower:]'`
- fi;
- # Evaluate the file name
- if [ "$TS_AS_FILENAME" == "TRUE" ]; then
- # Get date and times from EXIF stamp
- ETIME=`echo $DATETIME | awk -F' ' '{print $2}'`
- # Unix Formatted DATE and TIME - For feeding to date()
- UFDATE=`echo $EDATE | sed y/:/-/`
- # Unix DateSTAMP
- UDSTAMP=`date -d "$UFDATE $ETIME" +%s`
- echo " Will rename to $UDSTAMP.$EXT"
- MVCMD="/$UDSTAMP.$EXT"
- fi;
- # DIRectory NAME for the file move
- # sed issue for y command fix provided by thomas
- DIRNAME=`echo $EDATE | sed y-:-/-`
- echo -n " Moving to ${MOVETO}${DIRNAME}${MVCMD} ... "
- mkdir -p "${MOVETO}${DIRNAME}" && mv -b -f "$2" "${MOVETO}${DIRNAME}${MVCMD}"
- echo "done."
- echo ""
- exit
+  # Check for EXIF and process it
+  echo -n ": Checking EXIF... "
+  # DATETIME=`identify -verbose "$2" | grep "exif:DateTime:" | awk -F' ' '{print $2" "$3}'`
+  DATETIME=`exiftool -createdate "$2" | grep "Create Date" | awk -F' ' '{print $4" "$5}'`
+
+  if [[ "$DATETIME" == "" ]]; then
+    echo "not found."
+    if [[ $USE_LMDATE == "TRUE" ]]; then
+      # I am deliberately not using %Y here because of the desire to display the date/time
+      # to the user, though I could avoid a lot of post-processing by using it.
+      DATETIME=`stat --printf='%y' "$2" | awk -F. '{print $1}' | sed y/-/:/`
+      echo " Using LMDATE: $DATETIME"
+    else
+      echo " Moving to ./noexif/"
+      mkdir -p "${MOVETO}noexif" && mv -b -f "$2" "${MOVETO}noexif"
+      exit
+    fi;
+  else
+    echo "found: $DATETIME"
+  fi;
+
+  # The previous iteration of this script had a major bug which involved handling the
+  # renaming of the file when using TS_AS_FILENAME. The following sections have been
+  # rewritten to handle the action correctly as well as fix previously mangled filenames.
+  # FIXME: Collisions are not handled.
+  #
+  EDATE=`echo $DATETIME | awk -F' ' '{print $1}'`
+
+  # Evaluate the file extension
+  if [ "$USE_FILE_EXT" == "TRUE" ]; then
+    # Get the FILE type and lowercase it for use as the extension
+    EXT=`file -b "$2" | awk -F' ' '{print $1}' | tr '[:upper:]' '[:lower:]'`
+    
+	if [[ "${EXT}" == "jpeg" && "${JPEG_TO_JPG}" == "TRUE" ]]; then EXT="jpg"; fi;
+  else
+    # Lowercase and use the current extension as-is
+    EXT=`echo "$2" | awk -F. '{print $NF}' | tr '[:upper:]' '[:lower:]'`
+  fi;
+  
+  # Evaluate the file name
+  if [ "$TS_AS_FILENAME" == "TRUE" ]; then
+    # Get date and times from EXIF stamp
+    ETIME=`echo $DATETIME | awk -F' ' '{print $2}'`
+
+    # Unix Formatted DATE and TIME - For feeding to date()
+    UFDATE=`echo $EDATE | sed y/:/-/`
+
+    # Unix DateSTAMP
+#    UDSTAMP=`date -d "$UFDATE $ETIME" +%s`
+#    echo " Will rename to $UDSTAMP.$EXT"
+#    MVCMD="/$UDSTAMP.$EXT"
+
+    MVCMD="/${UFDATE}_${ETIME}.${EXT}"
+  fi;
+
+  # DIRectory NAME for the file move
+  # sed issue for y command fix provided by thomas
+#  DIRNAME=`echo $EDATE | sed y-:-/-`
+  DIRNAME=`date -d $UFDATE $DIRFORMAT `
+
+  echo -n " Moving to ${MOVETO}${DIRNAME}${MVCMD} ... "
+  mkdir -p "${MOVETO}${DIRNAME}" && mv --backup=numbered "$2" "${MOVETO}${DIRNAME}${MVCMD}"
+  echo "done."
+  echo ""
+  exit
 fi;
 #
 ###############################################################################
@@ -127,12 +145,12 @@ fi;
 # Could probably be optimized into a function instead, but I don't think there's an
 # advantage performance-wise. Suggestions are welcome at the URL at the top.
 for x in "${FILETYPES[@]}"; do
- # Check for the presence of imagemagick and the identify command.
+ # Check for the presence of exiftool command.
  # Assuming its valid and working if found.
- I=`which identify`
+ I=`which exiftool`
  if [ "$I" == "" ]; then
- echo "The 'identify' command is missing or not available."
- echo "Is imagemagick installed?"
+ echo "The 'exiftool' command is missing or not available."
+ echo "Is exiftool installed?"
  exit 1
  fi;
  echo "Scanning for $x..."
@@ -149,3 +167,4 @@ echo "done."
 echo -n "Cleaning up empty directories ... "
 find . -empty -delete
 echo "done."
+
